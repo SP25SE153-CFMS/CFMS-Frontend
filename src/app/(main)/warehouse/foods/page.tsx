f50dc13f7d22034bb742e23d41b290f57d3e5702
lachimo <lachimolalasii1410@gmail.com>
2025-03-03 23:24:59 +0700

    feat: add component search to search name and search code

--- src/app/(main)/warehouse/foods/page.tsx
+++ src/app/(main)/warehouse/foods/page.tsx
@@ -2,13 +2,27 @@
 import { DataTable } from '@/components/table/data-table';
 import { warehouseProduct } from '@/utils/data/table.data';
 import { columns } from './columns';
+import { Input } from '@/components/ui/input';
+import { useState } from 'react';
+import Search from '@/components/search/search';
 
 export default function Foods() {
+    const [searchValue, setSearchValue] = useState('');
+
+    const filteredData = warehouseProduct.filter(
+        (product) =>
+            product.productName.toLowerCase().startsWith(searchValue.toLowerCase()) ||
+            product.productCode.toLowerCase().includes(searchValue.toLowerCase()),
+    );
     return (
-        <div>
-            <h1 className="text-2xl font-bold tracking-tight pb-[20px]">Quản lý kho thức ăn</h1>
+        <div className="flex flex-col gap-y-[20px]">
+            <h1 className="text-2xl font-bold tracking-tight">Quản lý kho thức ăn</h1>
+            <div className="flex gap-x-[10px] justify-start items-center">
+                <p className="font-semibold whitespace-nowrap">Tìm kiếm: </p>
+                <Search onSearch={setSearchValue} />
+            </div>
             <div>
-                <DataTable data={warehouseProduct} columns={columns} />
+                <DataTable data={filteredData} columns={columns} />
             </div>
         </div>
     );
