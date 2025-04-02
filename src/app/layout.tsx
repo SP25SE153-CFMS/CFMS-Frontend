bb7863c098049959dcfe26b4c9689facd04ee26d
Hai Dang <contact@hdang09.tech>
2025-04-02 10:01:47 +0700

    chore: add open graph metaadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,11 +10,33 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
     title: {
         template: '%s | CFMS',
-        default: 'Hệ Thống Quản Lý Trang Trại Gà',
+        default: 'Hệ Thống Quản Lý Trang Trại Gà - CFMS',
     },
+    description:
+        'CFMS tối ưu hóa hoạt động, lịch cho ăn và nâng cao năng suất tổng thể của trang trại bằng cách theo dõi sức khỏe của gà',
     openGraph: {
-        title: 'CFMS',
-        description: 'Hệ Thống Quản Lý Trang Trại Gà',
+        url: 'http://cfms.hdang09.me',
+        type: 'website',
+        title: 'Hệ Thống Quản Lý Trang Trại Gà - CFMS',
+        description:
+            'CFMS tối ưu hóa hoạt động, lịch cho ăn và nâng cao năng suất tổng thể của trang trại bằng cách theo dõi sức khỏe của gà',
+        images: [
+            {
+                url: 'https://res.cloudinary.com/dvqtkzmr5/image/upload/v1743562518/txje0dm0e8h4z3rth0v8.png',
+                width: 1200,
+                height: 630,
+                alt: 'Hệ Thống Quản Lý Trang Trại Gà - CFMS',
+            },
+        ],
+    },
+    twitter: {
+        card: 'summary_large_image',
+        title: 'Hệ Thống Quản Lý Trang Trại Gà - CFMS',
+        description:
+            'CFMS tối ưu hóa hoạt động, lịch cho ăn và nâng cao năng suất tổng thể của trang trại bằng cách theo dõi sức khỏe của gà',
+        images: [
+            'https://res.cloudinary.com/dvqtkzmr5/image/upload/v1743562518/txje0dm0e8h4z3rth0v8.png',
+        ],
     },
 };
 
